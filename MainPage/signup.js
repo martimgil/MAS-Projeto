5348f6638662d0df9039a4639da97f5cb15c45e9
Martim Gil <martimgil@ua.pt>
2024-12-22 00:43:52 +0000

    correçoes, melhoria de consistencia na pagina de progresso.html, o botao de remover ficou mais clean e simpatico

---
+++ MainPage/signup.js
@@ -0,0 +1,18 @@
+function checkInfo(){
+    email = document.getElementById("email").value;
+    password = document.getElementById("password").value;
+
+    if (email === "claudioxavier@ua.pt") {
+        if (password === "claudiozito01") {
+            alert("Login efetuado com sucesso!");
+            window.location.href = "progresso.html";
+        }
+        else {
+            alert("Password incorreta!");
+        }
+    }
+    else {
+        alert("Email incorreto!");
+    }
+
+};
